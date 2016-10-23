9d84fdf7bf591cb71fdac13c4c721d18f9a56a2d
Maciej Krawczyk <wirus15@gmail.com>
2016-10-23 12:53:51 +0200

    managed to start the game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,7 +1,25 @@
+/// <reference path="../../../node_modules/phaser/typescript/phaser.comments.d.ts"/>
+
 import {Component} from "@angular/core";
+import * as Phaser from 'phaser';
 
 @Component({
     selector: 'b-game',
-    template: '<h1>Jedziem!</h1>'
+    template: '<div id="gameCanvas"></div>'
 })
-export class GameComponent { }
+export class GameComponent {
+    private game: Phaser.Game;
+
+    constructor() {
+        this.game = new Phaser.Game(
+            800, 600,         // width x height
+            Phaser.AUTO,      // the game context, 2D/3D
+            'gameCanvas',    // id of the DOM element to add the game
+            {
+                preload: function() {},
+                create: function() {},
+                update: function() {}
+            }
+        )
+    }
+}
--- src/vendor.ts
+++ src/vendor.ts
@@ -5,3 +5,6 @@ import '@angular/common';
 import '@angular/http';
 import '@angular/router';
 import 'rxjs';
+import 'pixi';
+import 'p2';
+import 'phaser';
